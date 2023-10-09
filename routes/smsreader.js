875dd284c25f4d8138da59e976880fabeaff862e
Jose Carlos Garza Alvarez <jcgarza@vivaro.com>
2023-10-09 11:43:18 -0600

    SMSREADER: se agrega parametro endpoint como string en log webhookSMS

--- routes/smsreader.js
+++ routes/smsreader.js
@@ -7,14 +7,14 @@ let pool
 router.post('/api/smsreader', async (req, res) => {
   pool = await db.getConn()
   try {
-    let logReq = await pool.request().query(`insert into SMSCargas.dbo.webhookSMS select getdate(),'${JSON.stringify(req.body)}',/api/smsreader`)
+    let logReq = await pool.request().query(`insert into SMSCargas.dbo.webhookSMS select getdate(),'${JSON.stringify(req.body)}','/api/smsreader'`)
     let { number, message, datetime } = req.body
     let insert = await pool
       .request()
       .query(`insert into SMSReportes.dbo.SMS_IN select '${message}','${number}','${datetime}','/api/smsreader',getdate()`)
     res.status(200).send({ desc: 'OK' })
   } catch (err) {
-    let logErr = await pool.request().query(`insert into SMSCargas.dbo.webhookSMS select getdate(),'${JSON.stringify(err)}',/api/smsreader`)
+    let logErr = await pool.request().query(`insert into SMSCargas.dbo.webhookSMS select getdate(),'${JSON.stringify(err)}','/api/smsreader'`)
     // console.log(err)
     res.status(500).send({ desc: 'error interno' })
   }
